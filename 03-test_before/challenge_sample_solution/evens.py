ce4248b6d4b6484c4d490ffbec48c52c766508ee
aaron <aaron@codeinstitute.net>
2018-09-19 16:37:49 +0100

    Modify solution for 03

--- 03-test_before/challenge_sample_solution/evens.py
+++ 03-test_before/challenge_sample_solution/evens.py
@@ -8,21 +8,6 @@ lesson.
 """
 
 
-def is_even(number):
-    """
-    A small helper function that simply checks to see if the number 
-    question is an even number, or an odd number.
-
-    `number` is the number in question
-
-    Returns `True` if the number is even, `False` if the number is odd
-    """
-    if number % 2 == 0:
-        return True
-    else:
-        return False
-
-
 def even_number_of_evens(numbers):
     """
     Returns the number of even numbers contained in a list of numbers.
@@ -30,37 +15,30 @@ def even_number_of_evens(numbers):
     `numbers` should be a list containing numbers
     
     Returns either True or False based on a number of criteria.
-        - if the length of the list is greater than 0, iterate over each item
-            to find out how many even numbers are in the list, and if the
-            number of even numbers is an even number, return `True`
-        - if the total number of even numbers in the list of 0, return `False`
-        - if the number of even numbers contained in the list is an odd
-            number, return `False`
-        - if the list contains no items, return `False`
+        - if `numbers` is empty, return `False`
+        - if the number of even numbers is odd, return `False`
+        - if the number of even numbers is 0, return `False`
+        - if the number of even numbers is even, return `True`
     """
 
-    # Check to see if the list is empty. If it is, return `False`, else start
-    # the iterative process
+    # Check to see if the list is empty
     if numbers == []:
         return False
     else:
         # Set a `number_of_evens` variable that will be incremented each time
         # an even number is found
-        number_of_evens = 0
-        for number in numbers:
-            if is_even(number):
-                number_of_evens += 1
+        evens = 0
         
-        # If no even numbers are found, return `False`
-        if number_of_evens == 0:
-            return False
-        
-        # If the number of even numbers found, is even, return `True`,
-        # else return `False`
-        if is_even(number_of_evens):
-            return True
-        else:
-            return False
+    # Iterate of over each item and if it's an even number, increment the
+    # `evens` variable
+    for number in numbers:
+        if number % 2 == 0:
+            evens += 1
+    
+    if evens == 0:
+        return False
+    else:
+        return evens % 2 == 0
 
 # Our set of test cases
 assert even_number_of_evens([]) == False, "No numbers"
