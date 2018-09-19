f9b578e9a74153ca6412d60d91f0c57d6f92a649
aaron <aaron@codeinstitute.net>
2018-09-19 12:35:35 +0100

    Implement Test After unit solutions

---
+++ 02-test_after/counts.py
@@ -0,0 +1,13 @@
+def count_upper_case(message):
+    count = 0
+    for c in message:
+        if c.isupper():
+            count += 1
+    return count
+
+assert count_upper_case("") == 0, "Empty string"
+assert count_upper_case("A") == 1, "One upper case"
+assert count_upper_case("a") == 0, "One lower case"
+assert count_upper_case("£$%%^") == 0, "Special characters"
+
+print("All the tests passed")
