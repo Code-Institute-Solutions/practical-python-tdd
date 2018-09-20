199abe3ef24657442a65572604ea0ce625e1d57d
aaron <aaron@codeinstitute.net>
2018-09-20 11:31:43 +0100

    Implement Vending Machine video solutions

---
+++ 08-vending_machine_part_two/video_solution/vending_machine.py
@@ -0,0 +1,46 @@
+from byotest import *
+
+usd_coins = [100, 50, 25, 10, 5, 2, 1]
+eur_coins = [100, 50, 20, 10, 5, 2, 1]
+
+
+def get_change(amount, coins=eur_coins):
+    """
+    Takes the payment amount and returns the change
+
+    `amount` the amount of money that we need to provide change for
+    `coins` is the set of coins that we need to get change for (i.e. the set
+        of available coins)
+
+    Returns a list of coin values
+    """
+    if amount == 0:
+        return []
+    
+    if amount in coins:
+        return [amount]
+    
+    change = []
+    for coin in coins:
+        if coin <= amount:
+            amount -= coin
+            change.append(coin)
+
+    return change
+
+
+#  Write our tests for our code
+test_are_equal(get_change(0), [])
+test_are_equal(get_change(1), [1])
+test_are_equal(get_change(2), [2])
+test_are_equal(get_change(5), [5])
+test_are_equal(get_change(10), [10])
+test_are_equal(get_change(20), [20])
+test_are_equal(get_change(50), [50])
+test_are_equal(get_change(100), [100])
+test_are_equal(get_change(3), [2, 1])
+test_are_equal(get_change(7), [5, 2])
+test_are_equal(get_change(9), [5, 2, 2])
+test_are_equal(get_change(35, usd_coins), [25, 10])
+
+print("All tests pass!")
